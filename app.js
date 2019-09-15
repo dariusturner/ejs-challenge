4c1f0027c62fa0721e0489a1f30f0394c358204d
Darius Turner <dariusot731@gmail.com>
2019-09-15 16:20:49 -0400

    Create object to contain the post title and body in the server - Complete

--- app.js
+++ app.js
@@ -32,8 +32,10 @@ app.get("/compose", function(req,res){
 });
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
-  console.log(req.body.postBody);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
 });
 
 
